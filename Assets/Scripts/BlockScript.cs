74069c07cf39928d54a802da1c94507f302102d7
HV-Gaming <harinesh@herovired.com>
2023-11-26 11:27:29 +0530

    V4

--- Assets/Scripts/BlockScript.cs
+++ Assets/Scripts/BlockScript.cs
@@ -18,31 +18,67 @@ public class BlockScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Mathf.Abs(rb.velocity.x) != 5f && Mathf.Abs(rb.velocity.z) != 5f)
+        {
+            // If not, set the velocity of the Rigidbody to zero
+            rb.velocity = Vector3.zero;
+        }
+
+        if (gameObject.CompareTag("LR"))
+        {
+            // If the tag is "LR", allow movement only in the X axis
+            rb.velocity = new Vector3(rb.velocity.x, 0f, 0f);
+        }
+        if (gameObject.CompareTag("UD"))
+        {
+            // If the tag is "UD", allow movement only in the Z axis
+            rb.velocity = new Vector3(0f, 0f, rb.velocity.z);
+        }
         
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         rb.velocity = Vector3.zero;
-        transform.position = new Vector3(Mathf.Round(transform.position.x),transform.position.y, Mathf.Round(transform.position.z));
+        rb.transform.position = new Vector3(Mathf.Round(transform.position.x),transform.position.y, Mathf.Round(transform.position.z));
         //Vector3 targetScale = new Vector3(0.8f, 0.8f, 0.8f);
 
-        if(transform.localScale != new Vector3(0.8f,0.8f,0.8f))
-        {
-            print("going here...");
-            transform.position = new Vector3 (Mathf.Round(transform.position.x*2)/2.0f,transform.position.y,Mathf.Round(transform.position.z*2)/2.0f);
-        }
-        // When a collision occurs, set the velocity to zero
-        // if(collision.gameObject.tag == "Player" || collision.gameObject.tag == "Shredder" )
+        // if(transform.localScale != new Vector3(0.8f,0.8f,0.8f))
         // {
-
-            
-            
-            
-
+        //     print("going here...");
+        //     transform.position = new Vector3 (Mathf.Round(transform.position.x*2)/2.0f,transform.position.y,Mathf.Round(transform.position.z*2)/2.0f);
         // }
+        // When a collision occurs, set the velocity to zero
+        if(collision.gameObject.tag == "LR" || collision.gameObject.tag == "UD" )
+        {
+            rb.velocity = Vector3.zero;
+
+            Vector3 roundedPosition = new Vector3(
+                Mathf.Round(transform.position.x * 2) / 2,
+                transform.position.y,
+                Mathf.Round(transform.position.z * 2) / 2);
+            
+            transform.position = roundedPosition;
+
+            
+            
+            
+
+        }
+
+
 
         
         //
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        // Check if the colliding GameObject has the tag "LR" or "UD"
+        if (collision.gameObject.CompareTag("LR") || collision.gameObject.CompareTag("UD") || collision.gameObject.CompareTag("Shredder"))
+        {
+            // Reset the position of this GameObject to its previous position
+            transform.position = transform.position;
+        }
     }
 }
